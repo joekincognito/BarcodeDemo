f68d1a3104ff7b529e4c263104d5c382cd93ec13
joekincognito <joekincognito@gmail.com>
2014-03-12 14:01:49 -0500

    ajax fix take 2

--- php/test.php
+++ php/test.php
@@ -1,14 +1,12 @@
 <?php
-	$qs = $_GET['qs'];
-	$qs = "9114901075742714812669";
+	$qs = $_GET['qs']; 
 	$data = array(
 		"9114901075742714812669" =>"12345",								
 		"4746904837" =>"99999",
 		"047469048372" =>"88888"	
 		);
-//return json_encode(returnData);
 	foreach ($data as $key => $value){
-		if($key==$qs){
+		if($key===$qs){
 			$returnData = $value;
 		}
 	}
