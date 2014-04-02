d6c2e13efe2ffa68eb7b28e5766248cef0177df1
joekincognito <joekincognito@gmail.com>
2014-04-02 15:23:42 -0500

    adsfdaf

--- js/orders.js
+++ js/orders.js
@@ -40,11 +40,15 @@ $('#update').click(function(){
         item.bercor=$(this).children().filter('#bercor').text();
         $('#log').append("<p>item.qty= " + item.qty + " and order.Id = " + order.Id + "item.bercor = " + item.bercor + " </p>" );
         db.transaction(function(tx){
-            tx.executeSql('update orderItems set qty=? where orderID=? and bercor=?',[item.qty,order.Id,item.bercor],null,errorCB);
+            tx.executeSql('update orderItems set qty=? where bercor=?',[item.qty,item.bercor],null,errorCB);
         },errorCB,updateSuccessCB);
+        /*db.transaction(function(tx){
+            tx.executeSql('update orderItems set qty=? where orderID=? and bercor=?',[item.qty,order.Id,item.bercor],null,errorCB);
+        },errorCB,updateSuccessCB);*/
     });
 });
 function updateSuccessCB(tx, results){
+    $('#log').append("updateSuccessCB");
     $('#log').append("Insert ID = " + results.insertId);
     $('#log').append("<br>");
     // this will be 0 since it is a select statement
