243f914857cc8f2295ccbea7d676baf26b78b50d
joekincognito <joekincognito@gmail.com>
2014-04-23 16:00:04 -0500

    tweeks15

--- js/inventory.js
+++ js/inventory.js
@@ -1,6 +1,10 @@
 var db;
 var gQTY; //global scoped qty
 var oh; //global scoped onHand qty field
+var gqmin; //global scoped min quantity
+var gqmax;//global scoped max quantity
+var min;//global scoped min field
+var max;//global scoped max field
 var item = {};
 $(document).ready(function() {
     // are we running in native app or in a browser?
@@ -67,9 +71,27 @@ function incInv(bercor,qty){
 $('#decInv').click(function(){
     bercor = $(this).parent().parent().children('.panel-body').children('.input-group').children('#bercor').val();
     qty = $(this).parent().parent().children('.panel-body').children('.form-group').children('#qty').val();
+    decInv(bercor,parseInt(qty));
 });
 function decInv(bercor,qty){
-    
+   db.transaction(
+            function(tx){
+                tx.executeSql('select onHand from inventory where bercor = ?',[bercor],
+            function(tx,results){
+                $('#log').append("<p>results rows length:"+results.rows.length+"</p>");
+                if (results.rows.length > 0){ 
+                    //if the bercor already exists, add to the qty
+                    newQty = results.rows.item(0).onHand - qty;
+                    $('#log').append("<p>newQty: " +newQty+"bercor: "+bercor+"</p>");
+                    tx.executeSql('update inventory set onHand=? where bercor=?',[parseInt(newQty), bercor]);
+                }
+                else
+                {
+                    //if the bercor does not exist, do nothing
+                    //tx.executeSql('insert into inventory(bercor, onhand) values(?,?)',[bercor,qty]);
+                }
+            },errorCB);
+        },errorCB,null);
 }
 /*********************************/
 /*********MIN MAX*****************/
@@ -78,31 +100,71 @@ $('#mmUpdate').click(function(){
     bercor = $(this).parent().parent().children('.panel-body').children('.input-group').children('#bercor').val();
     min = $(this).parent().parent().children('.panel-body').children('.form-group').children('#min').val();    
     max = $(this).parent().parent().children('.panel-body').children('.form-group').children('#max').val();    
+    setMinMax(bercor,min,max);
 });
 function setMinMax(bercor,min,max) {
-
+     db.transaction(
+            function(tx){
+                tx.executeSql('select * from inventory where bercor = ?',[bercor],
+            function(tx,results){
+                $('#log').append("<p>results rows length:"+results.rows.length+"</p>");
+                if (results.rows.length > 0){ 
+                    //if the bercor already exists, change onhand
+                    $('#log').append("<p>changing to min: "+min+"</p>");
+                    tx.executeSql('update inventory set min=?, max=? where bercor=?',[min, max, bercor]);
+                }
+                else
+                {
+                    tx.executeSql('insert into inventory(bercor, min, max) values(?,?,?)',[bercor, min, max]);
+                }                  
+            },errorCB);
+        },errorCB,null);
 }
 $('#mmCheck').click(function(){
     bercor = $(this).parent().parent().children('.panel-body').children('.input-group').children('#bercor').val();
-    
     min = $(this).parent().parent().children('.panel-body').children('.form-group').children('#min');    
-    max = $(this).parent().parent().children('.panel-body').children('.form-group').children('#max')();   
-    min.val(result.min);
-    max.val(result.max);
+    max = $(this).parent().parent().children('.panel-body').children('.form-group').children('#max');
+    getMinMax(bercor);       
 });
-function getMinMax(bercor,min,max) {
-
+function getMinMax(bercor) {
+    db.transaction(
+            function(tx){
+                tx.executeSql('select min,max from inventory where bercor = ?',[bercor],
+            function(tx,results){
+                $('#log').append("<p>results rows length:"+results.rows.length+"</p>");
+                if (results.rows.length > 0){ 
+                    //if the bercor already exists, change onhand
+                    gqmin = parseInt(result.rows.items(0).min);
+                    gqmax = parseInt(result.rows.items(0).max);
+                }             
+            },errorCB);
+        },errorCB,function(){min.val(gqmin);max.val(gqmax);});
 }
 /*********************************/
 /*********ON HAND*****************/
 /*********************************/
 $('#ohUpdate').click(function(){
     bercor = $(this).parent().parent().children('.panel-body').children('.input-group').children('#bercor').val();
-    oh = $(this).parent().parent().children('.panel-body').children('.form-group').children('#onHand');
-    //oh.val(result.onHand); 
+    oh = $(this).parent().parent().children('.panel-body').children('.form-group').children('#onHand').val();
+    setOH(bercor,parseInt(oh)); 
 });
 function setOH(bercor,qty) {
-
+     db.transaction(
+            function(tx){
+                tx.executeSql('select * from inventory where bercor = ?',[bercor],
+            function(tx,results){
+                $('#log').append("<p>results rows length:"+results.rows.length+"</p>");
+                if (results.rows.length > 0){ 
+                    //if the bercor already exists, change onhand
+                    $('#log').append("<p>changing to qty: "+qty+"</p>");
+                    tx.executeSql('update inventory set onHand=? where bercor=?',[qty, bercor]);
+                }
+                else
+                {
+                    tx.executeSql('insert into inventory(bercor, onhand) values(?,?)',[bercor,qty]);
+                }                  
+            },errorCB);
+        },errorCB,null);
 }
 $('#ohCheck').click(function(){
     bercor = $(this).parent().parent().children('.panel-body').children('.input-group').children('#bercor').val();
