a3c9f6c8774175d0a3b8e8f25eff8d2760549568
joekincognito <joekincognito@gmail.com>
2014-07-03 11:13:39 -0500

    start of shake feature 3

--- js/new-index.js
+++ js/new-index.js
@@ -56,7 +56,7 @@ $('#addToOrder').click(function(){
 function onDeviceReady() {
     $('#deviceready .listening').hide();
     $('#deviceready .received').show();
-    var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+     var scanner = cordova.require("cordova/plugin/BarcodeScanner");
     shake.startWatch(scan);
     $('#log').hide();
     if( window.isphone ) {
@@ -241,3 +241,66 @@ function ajax(number){ //number will bercor
                 $('#item').val( item.bercor);
             });    
 }
+var shake = (function () {
+    var shake = {},
+        watchId = null,
+        options = { frequency: 300 },
+        previousAcceleration = { x: null, y: null, z: null },
+        shakeCallBack = null;
+    
+    // Start watching the accelerometer for a shake gesture
+    shake.startWatch = function (onShake) {
+        if (onShake) {
+            shakeCallBack = onShake;
+        }
+        watchId = navigator.accelerometer.watchAcceleration(getAccelerationSnapshot, handleError, options);
+    };
+    
+    // Stop watching the accelerometer for a shake gesture
+    shake.stopWatch = function () {
+        if (watchId !== null) {
+            navigator.accelerometer.clearWatch(watchId);
+            watchId = null;
+        }
+    };
+    
+    // Gets the current acceleration snapshot from the last accelerometer watch
+    function getAccelerationSnapshot() {
+        navigator.accelerometer.getCurrentAcceleration(assessCurrentAcceleration, handleError);
+    }
+    
+    // Assess the current acceleration parameters to determine a shake
+    function assessCurrentAcceleration(acceleration) {
+        var accelerationChange = {};
+        if (previousAcceleration.x !== null) {
+            accelerationChange.x = Math.abs(previousAcceleration.x, acceleration.x);
+            accelerationChange.y = Math.abs(previousAcceleration.y, acceleration.y);
+            accelerationChange.z = Math.abs(previousAcceleration.z, acceleration.z);
+        }
+        if (accelerationChange.x + accelerationChange.y + accelerationChange.z > 30) {
+            // Shake detected
+            if (typeof (shakeCallBack) === "function") {
+                shakeCallBack();
+            }
+            shake.stopWatch();
+            setTimeout(shake.startWatch, 1000);
+            previousAcceleration = { 
+                x: null, 
+                y: null, 
+                z: null
+            }
+        } else {
+            previousAcceleration = {
+                x: acceleration.x,
+                y: acceleration.y,
+                z: acceleration.z
+            }
+        }
+    }
+
+    // Handle errors here
+    function handleError() {
+    }
+    
+    return shake;
+})();
