178a656d3e17106f1cf6d769f6bca58cfa98bfe0
joekincognito <joekincognito@gmail.com>
2014-07-21 13:33:09 -0500

    v1.5 improvement 2

--- js/view_orders.js
+++ js/view_orders.js
@@ -49,17 +49,24 @@ function getOrdersSuccess(tx, results) {
         $('#log').append("<p>Order table: " + len + " rows found.</p>");
         
         var name = results.rows.item(0).name;
-        $('#orderHistory').append('<p>'+ name + '</p>');
+        $('#orderHistory').append('<table><thead><tr><th>'+results.rows.item(0).date+'</th><th>Order Name</th><th>'+ name + '</th></tr>');
+        $('#orderHistory').append('<tr><th>QTY</th><th>Bercor</th><th>Desc</th></tr></thead><tbody>');
+        $('#orderHistory').append('<tr><td>'+results.rows.item(0).qty+'</td><td>'+results.rows.item(0).bercor+'</td><td>'+results.rows.item(0).desc+'</td></tr>');
         for (var i=0; i<len; i++){
             var result=results.rows.item(i);
             if (name == results.rows.item(i).name)
             {
-                $('#orderHistory').append('<span>'+ result.desc + '</span>');   
+                $('#orderHistory').append('<tr><td>'+results.rows.item(i).qty+'</td><td>'+results.rows.item(i).bercor+'</td><td>'+results.rows.item(i).desc+'</td></tr>');
+                //$('#orderHistory').append('<span>'+ result.desc + '</span>');   
             }
             else
             {
+                $('#orderHistory').append('</tbody></table>');
                 var name = result.name;
-                $('#orderHistory').append('<p>'+ name + '</p>');        
+                $('#orderHistory').append('<table><thead><tr><th>'+results.rows.item(i).date+'</th><th>Order Name</th><th>'+ name + '</th></tr>');
+                $('#orderHistory').append('<tr><th>QTY</th><th>Bercor</th><th>Desc</th></tr></thead><tbody>');
+                 $('#orderHistory').append('<tr><td>'+results.rows.item(0).qty+'</td><td>'+results.rows.item(0).bercor+'</td><td>'+results.rows.item(0).desc+'</td></tr>');
+                //$('#orderHistory').append('<p>'+ name + '</p>');        
             }
             //$('#orderHistory').append('<ul><li>'+ result.name + '</li></ul>');
 
